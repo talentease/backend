da5aa51cc980dad3da18f0b115be6f896a5dc840
razanfawwaz <razanfawwaz1905@gmail.com>
2023-05-27 15:48:16 +0700

    fix: fix firebase config

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 const serviceAccount = require(process.env.SERVICE_ACCOUNT_PATH);
 
-const app = admin.initializeApp({
+admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
