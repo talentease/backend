ec7bbdfcd9bc93c38be4d94e80438021391167d0
Muhammad Rizki Syafapri <67809355+mrsyafapri@users.noreply.github.com>
2023-05-29 00:36:11 +0700

    Dev/04 (#3)
    * feat: add configuration for ESLint
    
    * feat: create REST API for CRUD operations on Position

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,9 +1,9 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
 const serviceAccount = require(process.env.SERVICE_ACCOUNT_PATH);
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
