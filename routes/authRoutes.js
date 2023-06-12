b405476bf55d579e483768e8bee0c13b30febfd3
razanfawwaz <razanfawwaz1905@gmail.com>
2023-06-12 23:33:55 +0700

    fix: route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const { registerWithEmail } = require('../controllers/authController');
+const { createCompanyAdmin } = require('../controllers/companyController');
 
 const router = express.Router();
 
 router.post('/register', registerWithEmail);
+router.post('/register/company', createCompanyAdmin);
 
 module.exports = router;
